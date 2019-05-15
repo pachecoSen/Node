cb8a34dd4b159c48f529b67e40c98fbb49c21b66
pachecoSen <pachecodr2013@gmail.com>
2019-05-15 18:27:05 -0500

    Fin Basic

--- Bases de node/app.js
+++ Bases de node/app.js
@@ -1,5 +1,6 @@
-
 const argv  = require("./config/yargs");
+
+require('colors');
 
 const { geoTabla, geoFile } = require('./helper');
 
@@ -10,14 +11,14 @@ let outData = geoTabla(base, limit);
 switch (exec[0]) {
     case 'crear':
         geoFile(`tablas/tablas_${ base }.txt`, outData)
-            .then(res => console.log(res))
-            .catch(err => console.log(err));
+            .then(res => console.log(res.green))
+            .catch(err => console.log(err.red));
         break;
     
     case 'lister':
         console.log(outData);
         break;
     default:
-        console.log('Sin comando para executar');
+        console.log('Sin comando para executar'.yellow);
         break;
 }
